05b047f5815906e5edd5847b5fc0ccdab7f1ce17
Alecsander <alecsanderdias@hotmail.com>
2025-02-11 23:50:59 -0300

    Revert "Alterando a estrutura"
    This reverts commit 06432f05fb82258c12c3af90e04c3911a052a74d.

--- src/Prevision.tsx
+++ src/Prevision.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// const yesterdayPrevision:Number[] = [];
+// const todayPrevision:Number[] = [];
+
+
 export default function Prevision() {
     function yesterday(direction:boolean) {
         let res:Number[] = [];
